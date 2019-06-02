1083373fa79cd4f3b64a9cc9936fec122855b0b4
tariqjunaid <junaidtariq_91@yahoo.com>
2019-06-02 10:28:32 -0400

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,68 +1,67 @@
 class Person
-	attr_reader :name
-	attr_accessor :bank_account, :happiness, :hygiene
-	def initialize(name)
-		@name = name
-		@bank_account = 25
-		@happiness = 8
-		@hygiene = 8
-	end
-
-	def happiness=(num)
-		@happiness = num
-		@happiness = 10 if @happiness > 10
-		@happiness = 0 if @happiness < 0
-	end
-
-	def hygiene=(num)
-		@hygiene = num
-		@hygiene = 10 if @hygiene > 10
-		@hygiene = 0 if @hygiene < 0
-	end
-
-	def happy?
-		happiness > 7
-	end
-
-	def clean?
-		hygiene > 7
-	end
-
-	def get_paid(salary)
-		@bank_account += salary
-		return "all about the benjamins"
-	end
-
-	def take_bath
-		self.hygiene += 4
-		return "♪ Rub-a-dub just relaxing in the tub ♫"
-	end
-
-	def work_out
-		self.hygiene -= 3
-		self.happiness +=2
-		return "♪ another one bites the dust ♫"
-	end
-
-	def call_friend(friend)
-		self.happiness += 3
-		friend.happiness += 3
-		return "Hi #{friend.name}! It's #{self.name}. How are you?"
-	end
-
-	def start_conversation(person, topic)
-		if topic == "politics"
-			[self, person].each do |x| 
-				x.happiness -= 2
-			end
-			return "blah blah partisan blah lobbyist"
-		elsif topic == "weather"
-			[self, person].each do |y| 
-				y.happiness += 1
-			end
-			return "blah blah sun blah rain"
-		else
-			return "blah blah blah blah blah"
-		end
-	end
+  attr_reader :name
+  attr_accessor :bank_account, :happiness, :hygiene
+  def initialize(name)
+    @name = name
+    @bank_account = 25
+    @happiness = 8
+    @hygiene = 8
+  end
+  
+  def happiness=(num)
+    @happiness = num
+    @happiness = 10 if @happiness > 10
+    @happiness = 0 if @happiness < 0
+  end
+  
+  def hygiene=(num)
+    @hygiene = num
+    @hygiene = 10 if @hygiene > 10
+    @hygiene = 0 if @hygiene < 0
+  end
+  
+  def happy?
+    happiness > 7
+  end
+  
+  def clean?
+    hygiene > 7
+  end
+  
+  def get_paid(salary)
+    @bank_account += salary
+    return "all about the benjamins"
+  end
+  
+  def take_bath
+    self.hygiene += 4
+    return "♪ Rub-a-dub just relaxing in the tub ♫"
+  end
+  
+  def work_out
+    self.hygiene -= 3
+    self.happiness +=2
+    return "♪ another one bites the dust ♫"
+  end
+  
+  def call_friend(friend)
+    self.happiness += 3
+    friend.happiness += 3
+    return "Hi #{friend.name}! It's #{self.name}. How are you?"
+  end
+  def start_conversation(person, topic)
+    if topic == "politics"
+      [self, person].each do |x|
+        x.happiness -= 2
+      end
+      return "blah blah partisan blah lobbyist"
+    elsif topic == "weather"
+      [self, person].each do |y|
+        y.happiness += 1
+      end
+      return "blah blah sun blah rain"
+    else
+      return "blah blah blah blah blah"
+    end
+  end
 end
